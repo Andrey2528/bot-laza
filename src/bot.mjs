1488da456b3c2b8d142c66d79d059d07200d5361
Andrey2528 <55595617+Andrey2528@users.noreply.github.com>
2023-04-10 23:40:24 +0300

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -8,32 +8,13 @@ bot.setMyCommands([
   { command: '/help', description: 'Вопрос нам' }
 ])
 
-sequelize.authenticate()
-sequelize.sync()
-
-const admins = [
-  'anastasia_hryn',
-  'aprioricool',
-  'KseniaLaza',
-  'sdcgba'
-]
-
 bot.on('message', msg => {
-  const chatId = msg.chat.id
-  const text = msg.text
-  const user = msg.chat.username
-  const reply = msg.reply_to_message?.text
-  const caption = msg.reply_to_message?.caption
-  const reply_video = msg.reply_to_message?.video?.file_id
-  const message_id = msg.message_id
-  const photo = msg.reply_to_message?.photo
-  const photo_id = photo ? photo[0].file_id : null
   switch (text) {
     case '/start':
-      messageService.Hello(chatId)
+      "hello"
       break
     case '/help':
-      messageService.Help(chatId)
+       "help"
       break
     case `/send`:
       if (user && admins.indexOf(user) !== -1 && (reply_video || reply || photo_id)) {
@@ -42,20 +23,6 @@ bot.on('message', msg => {
       break
     default:
       user && admins.indexOf(user) == -1 ? adminService.sentToAdmin(message_id, chatId, user) : null
-      break
-  }
-})
-
-bot.on('callback_query', msg => {
-  const chatId = msg.message?.chat.id
-  const data = msg.data
-  const id = chatId ? chatId : -1
-  switch (data) {
-    case 'speed':
-      callbackService.Speed(id)
-      break
-    case 'buisness':
-      callbackService.Buisness(id)
       break
   }
 })
